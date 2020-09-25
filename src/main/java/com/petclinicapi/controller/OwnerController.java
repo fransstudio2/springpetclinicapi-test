0ef0eb9364370614d65eaee263dcd676abef3dc6
Fransiskus Winata <fransiskus.winata@tui.com>
2020-09-25 19:23:33 +0200

    first commit2

---
+++ src/main/java/com/petclinicapi/controller/OwnerController.java
@@ -0,0 +1,14 @@
+package com.petclinicapi.controller;
+
+// import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class OwnerController {
+
+  @GetMapping("/owners")
+  public String index() {
+    return "hallo owner";
+  }
+}
